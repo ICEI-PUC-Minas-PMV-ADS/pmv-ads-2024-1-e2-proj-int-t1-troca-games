9e86e3dc73a00b0bbcd84d4ebf1ad6756055c134
Priscila Maruno <priscilamiyukimaruno@gmail.com>
2024-05-07 21:14:03 +0900

    Add files via upload

---
+++ troca-games/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace troca_games.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Jogo> Jogos { get; set;}
+    }
+}
---
+++ troca-games/Models/Jogo.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace troca_games.Models
+{
+    [Table("Jogos")]
+    public class Jogo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        [Display(Name = "Jogo")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        [Display(Name = "Console")]
+        public string Console { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        [Display(Name = "Venda/Aluguel")]
+        public NegociacaoJogo NegociacaoJogo { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        [Display(Name = "Condição")]
+        public CondicaoJogo CondicaoJogo { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        [Display(Name = "Proprietário")]
+        public int UsuarioId { get; set; }
+
+        [ForeignKey("UsuarioId")]
+        [Display(Name = "Proprietário")]
+        public Usuario Usuario { get; set; }
+    }
+
+    public enum NegociacaoJogo
+    {
+        Venda, Aluguel
+    }
+
+    public enum CondicaoJogo
+    {
+        Novo, Usado
+    }
+}
